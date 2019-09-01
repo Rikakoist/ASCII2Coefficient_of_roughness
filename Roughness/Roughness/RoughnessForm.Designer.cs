7b35440ef0f084023d2823cce6295cbf143521f3
_mikuappend <13903476+Rikakoist@users.noreply.github.com>
2019-09-01 17:53:22 +0800

    Add files via upload

---
+++ Roughness/Roughness/RoughnessForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Roughness
+{
+    partial class RoughnessForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoughnessForm));
+            this.FilePathButton = new System.Windows.Forms.Button();
+            this.FilePathLabel = new System.Windows.Forms.Label();
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.FileContentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.NoDataValueTextBox = new System.Windows.Forms.TextBox();
+            this.RoughnessLabel = new System.Windows.Forms.Label();
+            this.RoughnessTextBox = new System.Windows.Forms.TextBox();
+            this.ConvertProgressBar = new System.Windows.Forms.ProgressBar();
+            this.NoDataLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FilePathButton
+            // 
+            this.FilePathButton.Location = new System.Drawing.Point(443, 31);
+            this.FilePathButton.Name = "FilePathButton";
+            this.FilePathButton.Size = new System.Drawing.Size(75, 23);
+            this.FilePathButton.TabIndex = 0;
+            this.FilePathButton.Text = "浏览...";
+            this.FilePathButton.UseVisualStyleBackColor = true;
+            this.FilePathButton.Click += new System.EventHandler(this.GetFilePath);
+            // 
+            // FilePathLabel
+            // 
+            this.FilePathLabel.AutoSize = true;
+            this.FilePathLabel.Location = new System.Drawing.Point(30, 36);
+            this.FilePathLabel.Name = "FilePathLabel";
+            this.FilePathLabel.Size = new System.Drawing.Size(55, 13);
+            this.FilePathLabel.TabIndex = 2;
+            this.FilePathLabel.Text = "文件路径";
+            // 
+            // FilePathTextBox
+            // 
+            this.FilePathTextBox.Location = new System.Drawing.Point(91, 33);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.Size = new System.Drawing.Size(342, 20);
+            this.FilePathTextBox.TabIndex = 1;
+            // 
+            // FileContentRichTextBox
+            // 
+            this.FileContentRichTextBox.Location = new System.Drawing.Point(250, 78);
+            this.FileContentRichTextBox.Name = "FileContentRichTextBox";
+            this.FileContentRichTextBox.ReadOnly = true;
+            this.FileContentRichTextBox.Size = new System.Drawing.Size(268, 133);
+            this.FileContentRichTextBox.TabIndex = 3;
+            this.FileContentRichTextBox.TabStop = false;
+            this.FileContentRichTextBox.Text = "";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(44, 206);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(145, 37);
+            this.ConvertButton.TabIndex = 4;
+            this.ConvertButton.Text = "开始转换！";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.GetR);
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.AutoSize = true;
+            this.ProgressLabel.Location = new System.Drawing.Point(322, 230);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(127, 13);
+            this.ProgressLabel.TabIndex = 4;
+            this.ProgressLabel.Text = "小老弟，你怎么回事？";
+            this.ProgressLabel.Click += new System.EventHandler(this.ShowAuthor);
+            // 
+            // NoDataValueTextBox
+            // 
+            this.NoDataValueTextBox.Location = new System.Drawing.Point(139, 95);
+            this.NoDataValueTextBox.Name = "NoDataValueTextBox";
+            this.NoDataValueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NoDataValueTextBox.TabIndex = 2;
+            // 
+            // RoughnessLabel
+            // 
+            this.RoughnessLabel.AutoSize = true;
+            this.RoughnessLabel.Location = new System.Drawing.Point(29, 140);
+            this.RoughnessLabel.Name = "RoughnessLabel";
+            this.RoughnessLabel.Size = new System.Drawing.Size(104, 13);
+            this.RoughnessLabel.TabIndex = 4;
+            this.RoughnessLabel.Text = "将非NoData值设为";
+            // 
+            // RoughnessTextBox
+            // 
+            this.RoughnessTextBox.Location = new System.Drawing.Point(139, 137);
+            this.RoughnessTextBox.Name = "RoughnessTextBox";
+            this.RoughnessTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RoughnessTextBox.TabIndex = 3;
+            // 
+            // ConvertProgressBar
+            // 
+            this.ConvertProgressBar.Location = new System.Drawing.Point(250, 263);
+            this.ConvertProgressBar.Name = "ConvertProgressBar";
+            this.ConvertProgressBar.Size = new System.Drawing.Size(268, 23);
+            this.ConvertProgressBar.Step = 1;
+            this.ConvertProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.ConvertProgressBar.TabIndex = 5;
+            // 
+            // NoDataLabel
+            // 
+            this.NoDataLabel.AutoSize = true;
+            this.NoDataLabel.Location = new System.Drawing.Point(53, 98);
+            this.NoDataLabel.Name = "NoDataLabel";
+            this.NoDataLabel.Size = new System.Drawing.Size(56, 13);
+            this.NoDataLabel.TabIndex = 4;
+            this.NoDataLabel.Text = "NoData值";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Enabled = false;
+            this.CancelButton.Location = new System.Drawing.Point(44, 249);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(145, 37);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "取消操作";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.GetR);
+            // 
+            // RoughnessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 306);
+            this.Controls.Add(this.ConvertProgressBar);
+            this.Controls.Add(this.RoughnessTextBox);
+            this.Controls.Add(this.RoughnessLabel);
+            this.Controls.Add(this.NoDataValueTextBox);
+            this.Controls.Add(this.NoDataLabel);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.FileContentRichTextBox);
+            this.Controls.Add(this.FilePathTextBox);
+            this.Controls.Add(this.FilePathLabel);
+            this.Controls.Add(this.FilePathButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.MaximizeBox = false;
+            this.Name = "RoughnessForm";
+            this.Text = "DEM转糙率工具";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ExitApp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FilePathButton;
+        private System.Windows.Forms.Label FilePathLabel;
+        private System.Windows.Forms.TextBox FilePathTextBox;
+        private System.Windows.Forms.RichTextBox FileContentRichTextBox;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.TextBox NoDataValueTextBox;
+        private System.Windows.Forms.Label RoughnessLabel;
+        private System.Windows.Forms.TextBox RoughnessTextBox;
+        private System.Windows.Forms.ProgressBar ConvertProgressBar;
+        private System.Windows.Forms.Label NoDataLabel;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
+
---
+++ Roughness/Roughness/RoughnessForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Roughness
+{
+    public partial class RoughnessForm : Form
+    {
+        public RoughnessForm()
+        {
+            InitializeComponent();
+        }
+        Thread BgCal;
+
+        //用户选择文件路径
+        private void GetFilePath(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog OFD = new OpenFileDialog
+                {
+                    InitialDirectory = Environment.SpecialFolder.Desktop.ToString(),
+                    CheckFileExists = true,
+                    Filter = "asc Files (*.asc)|*.asc",
+                    FilterIndex = 1,
+                };
+                if (OFD.ShowDialog() == DialogResult.OK)
+                {
+                    FilePathTextBox.Text = OFD.FileName;
+                    using (StreamReader sr = new StreamReader(FilePathTextBox.Text))
+                    {
+                        for (int i = 0; i < 6; i++)
+                        {
+                            string CurrentLine = sr.ReadLine();
+                            if (i == 5)
+                            {
+                                double TLine = Convert.ToDouble(CurrentLine.Substring(14).Replace("\r\n", "")); //自动读取NoDATA值
+                                NoDataValueTextBox.Text = TLine.ToString();
+                            }
+                            FileContentRichTextBox.Text += CurrentLine + "\r\n";
+                            this.Refresh();
+                        }
+                        using (StreamWriter sw = new StreamWriter(FilePathTextBox.Text.Replace(".asc", "_out.asc")))
+                        {
+                            sw.Write(FileContentRichTextBox.Text);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void ExitApp(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //转换
+        private void GetR(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sender == ConvertButton)
+                {
+                    BgCal = new Thread(Calc);
+                    BgCal.Priority = ThreadPriority.Normal;
+                    BgCal.Start();  //在新线程上执行转换任务，不会卡界面
+                    ConvertButton.Enabled = false;
+                    CancelButton.Enabled = true;
+                }
+                if (sender == CancelButton)
+                {
+                    if (BgCal.IsAlive)
+                    {
+                        BgCal.Abort();
+                        ConvertButton.Enabled = true;
+                        CancelButton.Enabled = false;
+                        ProgressLabel.Text = "你烟了";
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                ConvertButton.Enabled = true;
+                CancelButton.Enabled = false;
+            }
+        }
+
+        //多线程调用方法
+        private void Calc()
+        {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(RoughnessTextBox.Text) || String.IsNullOrWhiteSpace(NoDataValueTextBox.Text))
+                {
+                    throw new FormatException("输入的糙率或NoData值不正确！");
+                }
+                string CurrentLine = null;
+                double CLine = 0;
+                double TLine = 0;
+
+                using (StreamReader sr = new StreamReader(FilePathTextBox.Text))
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                        CurrentLine = sr.ReadLine();
+                        if (i == 1)
+                        {
+                            TLine = Convert.ToInt32(CurrentLine.Substring(14).Replace("\r\n", "")); //自动读取行数
+                        }
+                    }
+                    using (StreamWriter sw = new StreamWriter(FilePathTextBox.Text.Replace(".asc", "_out.asc")))
+                    {
+                        sw.Write(FileContentRichTextBox.Text);
+                    }
+
+                    while ((CurrentLine = sr.ReadLine()) != null)   //读取直到文档末尾
+                    {
+                        string[] DEMValues = CurrentLine.Split(' ');
+                        string Converted = "0";
+                        for (int j = 0; j < DEMValues.Length - 1; j++)
+                        {
+                            if (DEMValues[j] != NoDataValueTextBox.Text)
+                            {
+                                DEMValues[j] = RoughnessTextBox.Text;
+                            }
+                            if (j == 0)
+                            {
+                                Converted = DEMValues[j] + " ";
+                            }
+                            else
+                            {
+                                Converted += DEMValues[j] + " ";
+                            }
+                        }
+                        using (StreamWriter sw = new StreamWriter(FilePathTextBox.Text.Replace(".asc", "_out.asc"), true))
+                        {
+                            sw.WriteLine(Converted);
+                            CLine++;
+                            ConvertProgressBar.Value = (CLine / TLine) * 100 <= 100 ? Convert.ToInt32((CLine / TLine) * 100) : 50;
+                            ProgressLabel.Text = "Processing: " + CLine.ToString() + " / " + TLine.ToString();
+                            this.Refresh();
+                            if (CLine >= TLine)
+                            {
+                                ConvertButton.Enabled = true;
+                                CancelButton.Enabled = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                ConvertButton.Enabled = true;
+                CancelButton.Enabled = false;
+            }
+        }
+
+        private void ShowAuthor(object sender, EventArgs e)
+        {
+            MessageBox.Show("DEM转糙率程序，由Rikakoist制作。更多工具请访问https://github.com/Rikakoist。", "2333~", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+    }
+}
